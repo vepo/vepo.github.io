25d3b1421fc3d6aa68ddf981ab87a8a8c2337370
Victor Osório <victor.osorio@openet.com>
2020-05-18 20:55:56 -0300

    New Content

--- _plugins/get_domain.rb
+++ _plugins/get_domain.rb
@@ -1,19 +1,10 @@
-module Jekyll
-    class GetDomainTag < Liquid::Tag
-  
-      def initialize(tag_name, text, tokens)
-        super
-        url = "http://#{text}" unless text.start_with?('http')
+module GetDomainFilter 
+    def get_domain(input)
+        url = "http://#{input}" unless input.start_with?('http')
         uri = URI.parse(url)
         host = uri.host.downcase
-        host.start_with?('www.') ? host[4..-1] : host
-        @text = host
-      end
-  
-      def render(context)
-        "#{@text}"
-      end
+        host.start_with?('www.') ? "#{host[4..-1]}" : "#{host}"
     end
-  end
-  
-  Liquid::Template.register_tag('get_domain', Jekyll::GetDomainTag)
+end
+
+Liquid::Template.register_filter(GetDomainFilter)
