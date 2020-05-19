e74c48ee615d481d7f280353a16047e48f2b7284
Victor Osório <victor.osorio@openet.com>
2020-05-18 21:49:09 -0300

    New Content

--- _plugins/get_domain.filter.rb
+++ _plugins/get_domain.filter.rb
@@ -3,9 +3,7 @@ require 'liquid'
 module Jekyll
     module GetDomainFilter 
         def get_domain(input)
-            url = input.start_with?('http') ? input : "http://#{input}"
-            uri = URI.parse(url)
-            host = uri.host.downcase
+            host = URI.parse(input.start_with?('http') ? input : "http://#{input}").host.downcase
             host.start_with?('www.') ? host[4..-1] : host
         end
     end
