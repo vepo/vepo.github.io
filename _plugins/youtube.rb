248a1ef6253651bdf2d1f6a8b5828ada7f3dd171
Victor Osório <victor.perticarrari@gmail.com>
2020-05-20 00:22:22 -0300

    New Content

---
+++ _plugins/youtube.rb
@@ -0,0 +1,17 @@
+require 'rest-client'
+require 'json'
+
+module Jekyll
+    class YoutubeTag < Liquid::Tag
+        def initialize(tag_name, youtube_id, tokens)
+            super
+            @youtube_id = youtube_id
+        end
+
+        def render(context)
+            "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/#{@youtube_id}\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        end
+    end
+end
+
+Liquid::Template.register_tag('youtube', Jekyll::YoutubeTag)
