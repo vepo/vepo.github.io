ec20d1e1897a4cdb5639ec894f8e4556b31e97c4
Victor Osório <victor.perticarrari@gmail.com>
2020-05-19 23:47:41 -0300

    New Content

---
+++ _plugins/gist.rb
@@ -0,0 +1,17 @@
+require 'rest-client'
+require 'json'
+
+module Jekyll
+    class GistTag < Liquid::Tag
+        def initialize(tag_name, gist_url, tokens)
+            super
+            @gist_url = gist_url.strip
+        end
+
+        def render(context)
+            "<script src=\"#{@gist_url}.js\"></script>"
+        end
+    end
+end
+
+Liquid::Template.register_tag('gist', Jekyll::GistTag)
