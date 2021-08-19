5b310ea8cce35a692cba7b8e9fc9f32599e32dcf
Victor Osório <victor.osorio@openet.com>
2021-08-18 22:13:37 -0300

    Ajustes no layout

---
+++ assets/js/adjustments.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+    $("p > img").each((index, elm) => {
+        if (elm.parentElement.children.length == 1) {
+            $(elm.parentElement).addClass("img-block");
+        }
+    });
+    $("article > table").each((index, elm) => {
+        var tableContainer = document.createElement('div');
+        tableContainer.classList.add('table-container');
+        elm.parentNode.insertBefore(tableContainer, elm);
+        tableContainer.appendChild(elm);
+    });
+});
