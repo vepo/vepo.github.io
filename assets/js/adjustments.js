5ae66fcb8c841791b5532ae49c51aa03e6e82d3c
Victor Osório <vepo@tutanota.com>
2021-09-10 11:25:08 -0300

    Centralizando vídeo

--- assets/js/adjustments.js
+++ assets/js/adjustments.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
         }
     });
     $('p > a > img').each((index, elm) => {
-        if($(elm).text().length == 0) {
+        if($(elm.parentElement.parentElement).text().length == 0) {
             $(elm.parentElement.parentElement).addClass("img-block");
         }
     });
@@ -15,5 +15,12 @@ $(document).ready(() => {
         tableContainer.classList.add('table-container');
         elm.parentNode.insertBefore(tableContainer, elm);
         tableContainer.appendChild(elm);
+    });
+
+    $("article > iframe").each((index, elm) => {
+        var iframeContainer = document.createElement('div');
+        iframeContainer.classList.add('img-block');
+        elm.parentNode.insertBefore(iframeContainer, elm);
+        iframeContainer.appendChild(elm);
     });
 });
