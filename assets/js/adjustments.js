b1f788ca23731db77768db46dd9e3bb573cfe93f
Victor Osório <victor.osorio@openet.com>
2022-11-24 10:45:57 -0300

    Update adjustments.js

--- assets/js/adjustments.js
+++ assets/js/adjustments.js
@@ -1,3 +1,20 @@
+navigator.sayswho= (function(){
+    var ua= navigator.userAgent;
+    var tem; 
+    var M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+})();
+
 $(document).ready(() => {
     $("p > img").each((index, elm) => {
         if (elm.parentElement.children.length == 1) {
@@ -23,4 +40,6 @@ $(document).ready(() => {
         elm.parentNode.insertBefore(iframeContainer, elm);
         iframeContainer.appendChild(elm);
     });
+    
+    $("body").addClass("browser", navigator.sayswho);
 });
