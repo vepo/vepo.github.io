9e9e19d4acad4ffd7be55bec0e9a20a49899322a
Victor Osorio <victor.osorio@amdocs.com>
2024-11-20 19:51:15 -0300

    Novo layout

--- _plugins/github.rb
+++ _plugins/github.rb
@@ -11,30 +11,30 @@ module Jekyll
             ##    -H "X-GitHub-Api-Version: 2022-11-28" \
             ##    https://api.github.com/repos/OWNER/REPO
             super
-            repo = repo.start_with?('https://github.com/') ? repo[19..-1] : repo
-            github_token = ENV["MY_GITHUB_SECRET"]
-            @repo_info = JSON.parse(::RestClient.get("https://api.github.com/repos/#{repo.strip}", {:authorization => "Bearer #{github_token}", :accept => "application/vnd.github+json", :'x-gitHub-api-version' => '2022-11-28'}))
-            puts @repo_info
+            # repo = repo.start_with?('https://github.com/') ? repo[19..-1] : repo
+            # github_token = ENV["MY_GITHUB_SECRET"]
+            # @repo_info = JSON.parse(::RestClient.get("https://api.github.com/repos/#{repo.strip}", {:authorization => "Bearer #{github_token}", :accept => "application/vnd.github+json", :'x-gitHub-api-version' => '2022-11-28'}))
+            # puts @repo_info
         end
 
         def render(context)
-            output = <<~EOS
-            <div class="github-repo">
-                <h2>
-                    <img class="logo" src="/assets/images/plugins/github-logo.png" alt="GitHub logo" loading="lazy" />
-                    <img class="user" src="#{@repo_info["owner"]["avatar_url"]}" loading="lazy" />
-                    <span class="url">
-                        <a href="https://github.com/#{@repo_info["owner"]["login"]}">
-                            #{@repo_info["owner"]["login"]}
-                        </a> 
-                        /
-                        <a href="https://github.com/#{@repo_info["owner"]["login"]}/#{@repo_info["name"]}">
-                            #{@repo_info["name"]}
-                        </a>
-                    </span>
-                </h2>
-            </div>
-            EOS
+            # output = <<~EOS
+            # <div class="github-repo">
+            #     <h2>
+            #         <img class="logo" src="/assets/images/plugins/github-logo.png" alt="GitHub logo" loading="lazy" />
+            #         <img class="user" src="#{@repo_info["owner"]["avatar_url"]}" loading="lazy" />
+            #         <span class="url">
+            #             <a href="https://github.com/#{@repo_info["owner"]["login"]}">
+            #                 #{@repo_info["owner"]["login"]}
+            #             </a> 
+            #             /
+            #             <a href="https://github.com/#{@repo_info["owner"]["login"]}/#{@repo_info["name"]}">
+            #                 #{@repo_info["name"]}
+            #             </a>
+            #         </span>
+            #     </h2>
+            # </div>
+            # EOS
         end
     end
 end
