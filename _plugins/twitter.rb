4b71b216f0ab1a4b2dc685aa80b1cec90c2d4063
Victor Osorio <victor.osorio@amdocs.com>
2024-11-20 21:44:05 -0300

    Twitter

--- _plugins/twitter.rb
+++ _plugins/twitter.rb
@@ -5,11 +5,11 @@ module Jekyll
     class TwitterTag < Liquid::Tag
         def initialize(tag_name, tweet_url, tokens)
             super
-            @tweet = JSON.parse(::RestClient.get("https://publish.twitter.com/oembed?theme=dark&align=center&url=#{ERB::Util.url_encode(tweet_url)}"))
+            # @tweet = JSON.parse(::RestClient.get("https://publish.twitter.com/oembed?theme=dark&align=center&url=#{ERB::Util.url_encode(tweet_url)}"))
         end
 
         def render(context)
-            @tweet['html']
+            # @tweet['html']
         end
     end
 end
