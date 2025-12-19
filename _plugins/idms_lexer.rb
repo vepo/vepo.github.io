373b1460d5cd23dc6a505c95a551043bc90a2e72
Victor Osorio <Victor.Osorio@amdocs.com>
2025-12-19 12:04:02 -0300

    Introducao a base de dados

---
+++ _plugins/idms_lexer.rb
@@ -0,0 +1,19 @@
+module Rouge
+  module Lexers
+    class IDMS < RegexLexer
+      title 'IDMS DML'
+      desc 'IDMS Data Manipulation Language'
+      tag 'idms'
+      filenames '*.idms'
+
+      state :root do
+        rule %r/\s+/, Text::Whitespace
+        rule %r/\/\*.*?\*\//m, Comment::Multiline
+        rule %r/"[^"]*"/, Str::Double
+        rule %r/\b(PROCEDURE|BEGIN|END|DECLARE|FIND|FIRST|NEXT|OWNER|WITHIN|OF|ON|ERROR|DISPLAY|AND|EXIT|REPEAT|USING|SET|IF|THEN|ELSE)\b/i, Keyword
+        rule %r/\b[A-Z_][A-Z0-9_]*\b/, Name
+        rule %r/[;=.]/, Punctuation
+      end
+    end
+  end
+end
